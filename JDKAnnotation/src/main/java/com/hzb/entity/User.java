16a9066112419589414c3870e90e9c1799276e6a
houzhibo <1651202167@qq.com>
2019-02-13 09:17:35 +0800

    反射，自定义注解

---
+++ JDKAnnotation/src/main/java/com/hzb/entity/User.java
@@ -0,0 +1,38 @@
+package com.hzb.entity;
+
+/**
+ * @author: houzhibo
+ * @date: 2019年2月12日 下午5:10:24
+ * @describe:
+ */
+public class User {
+
+	private String name="zhansan";
+	private String age;
+	private String sex;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+}
